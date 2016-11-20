3be3eb29e52e6f1a3d05d4822c51a3393c98f614
David Skoglund <dskoggz@gmail.com>
2016-11-19 19:34:56 -0800

    user can use services dropdown and see each individual service template.

--- public/default.js
+++ public/default.js
@@ -10,20 +10,45 @@ app.config(['$routeProvider', '$locationProvider',
         controller: 'HomeController',
         controllerAs: 'home'
       })
-      .when('/ServicesWeProvide', {
-        templateUrl: '/templates/services.html',
-        controller: 'ServicesController',
+      .when('/ServicesWeProvide/Plumbing', {
+        templateUrl: '/templates/services/plumbing.html',
+        controller: 'NavigationController',
+        controllerAs: 'services'
+      })
+      .when('/ServicesWeProvide/ResidentialFireSprinklers', {
+        templateUrl: '/templates/services/residentialfiresprinklers.html',
+        controller: 'NavigationController',
+        controllerAs: 'services'
+      })
+      .when('/ServicesWeProvide/CommercialFireSprinklers', {
+        templateUrl: '/templates/services/commercialfiresprinklers.html',
+        controller: 'NavigationController',
+        controllerAs: 'services'
+      })
+      .when('/ServicesWeProvide/IrrigationSprinklerSystems', {
+        templateUrl: '/templates/services/irrigationsprinklersystems.html',
+        controller: 'NavigationController',
+        ContactController: 'services'
+      })
+      .when('/ServicesWeProvide/BackFlowServices', {
+        templateUrl: '/templates/services/backflowservices.html',
+        controller: 'NavigationController',
+        controllerAs: 'services'
+      })
+      .when('/ServicesWeProvide/WaterHeaters', {
+        templateUrl: '/templates/services/waterheaters.html',
+        controller: 'NavigationController',
         controllerAs: 'services'
       })
       .when('/CustomerTestimonials', {
         templateUrl: '/templates/testimonials.html',
-        controller: 'TestimonialsController',
+        controller: 'NavigationController',
         controllerAs: 'testimonials'
       })
       .when('/ContactUs', {
         templateUrl: '/templates/contact.html',
-        controller: 'ContactController',
-        controllerAs: 'contact'
+        controller: 'NavigationController',
+        controllerAs: 'contacts'
       })
       $locationProvider.html5Mode({
         enable: true,
@@ -58,10 +83,33 @@ function NavigationController($scope, $location, $anchorScroll, $window) {
     $location.path('/ServicesWeProvide')
     scroll()
   }
+  $scope.goToPlumbingView = function() {
+    $location.path('/ServicesWeProvide/Plumbing')
+    scroll()
+  }
+  $scope.goToResidentialFireSprinklersView = function() {
+    $location.path('/ServicesWeProvide/ResidentialFireSprinklers')
+    scroll()
+  }
+  $scope.goToCommercialFireSprinklersView = function() {
+    $location.path('/ServicesWeProvide/CommercialFireSprinklers')
+    scroll()
+  }
+  $scope.goToIrrigationSprinklerSystemsView = function() {
+    $location.path('/ServicesWeProvide/IrrigationSprinklerSystems')
+    scroll()
+  }
+  $scope.goToBackFlowServicesView = function() {
+    $location.path('/ServicesWeProvide/BackFlowServices')
+    scroll()
+  }
+  $scope.goToWaterHeatersView = function() {
+    $location.path('/ServicesWeProvide/WaterHeaters')
+    scroll()
+  }
   $scope.goToTestimonialView = function() {
     $location.path('/CustomerTestimonials')
     scroll()
-
   }
   $scope.goToContactView = function() {
     $location.path('/ContactUs')
